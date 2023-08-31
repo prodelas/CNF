b5f1493efdda75882d424b7bf07947e25ab3a9d0
Pedro González Rodelas <prodelas@gmail.com>
2023-08-31 18:47:40 +0200

    Update and rename Formulas.hs to Formulas_es.hs
    Versión del código con los comentarios en español.

--- Formulas_es.hs
+++ Formulas_es.hs
@@ -1,7 +1,7 @@
 module Formulas (module Formulas) where
 
 {- Definimos el tipo de dato Formula, que refleja la escritura de las formulas en forma polaca. 
-Las fórmulas atóminas están basadas en números enteros (Int). -}
+Las fÃ³rmulas atÃ³minas estÃ¡n basadas en nÃºmeros enteros (Int). -}
 
 data Formula = Fa Int 
              | C Formula Formula 
@@ -12,7 +12,7 @@ data Formula = Fa Int
                deriving (Eq, Ord)
 
 {-
-Varias fórmulas para hacer pruebas
+Varias fÃ³rmulas para hacer pruebas
 -}
 
 u10 = K (Fa 10)(Fa 11)
@@ -30,11 +30,11 @@ u1 = K (Fa 1) u2
 x1=E (Fa 1) (Fa 2) :: Formula  -- x1 = p1 <-> p2
 x2=K (Fa 2) (Fa 1) :: Formula  -- x2 = p2 ^ p1
 x3=C (Fa 1) (Fa 2) :: Formula  -- x3 = p1 -> p2
-x4=N x3 :: Formula  -- ¬(p1 -> p2)
-x5=A x4 x2 :: Formula -- ¬(p1 -> p2) v (p1 <-> p2) 
-x6=N x5 :: Formula  -- ¬(¬(p1 -> p2) v (p1 <-> p2))
-x7=N x1 :: Formula -- ¬(p1 <-> p2)
-x=A x6 x7 :: Formula -- ¬(¬(p1 -> p2) v (p1 <-> p2)) v ¬(p1 <-> p2)
+x4=N x3 :: Formula  -- Â¬(p1 -> p2)
+x5=A x4 x2 :: Formula -- Â¬(p1 -> p2) v (p1 <-> p2) 
+x6=N x5 :: Formula  -- Â¬(Â¬(p1 -> p2) v (p1 <-> p2))
+x7=N x1 :: Formula -- Â¬(p1 <-> p2)
+x=A x6 x7 :: Formula -- Â¬(Â¬(p1 -> p2) v (p1 <-> p2)) v Â¬(p1 <-> p2)
 
 {-
 y1 = se x
@@ -67,14 +67,14 @@ z17= K (Fa 17) z16
 z18= K (Fa 18) z17
 
 
-{- Crea una fórmula formada únicamente por conjunciones -}
+{- Crea una fÃ³rmula formada Ãºnicamente por conjunciones -}
 
 f :: Int -> Int -> Formula
 f 0 n = K (Fa n) (N (Fa n))
 f m n = K (f (m -1) n) (f (m-1) (n+2^m))
 
 
-{- Algunos ejemplos de tautologías -}
+{- Algunos ejemplos de tautologÃ­as -}
 
 taut1::Formula
 taut2::Formula
@@ -102,7 +102,7 @@ taut2 = C (C (C (C (Fa 1) (Fa 2)) (Fa 2)) (C (Fa 3) (Fa 2))) (C (Fa 1) (C (Fa 3)
 taut3 = C (C (C (C (C (Fa 1) (Fa 2)) (C (N (Fa 3)) (N (Fa 4)))) (Fa 3)) (Fa 5)) (C (C (Fa 5) (Fa 1)) (C (Fa 4) (Fa 1))) 
 
 taut31 = C (C (C (C (Fa 1) (Fa 2)) (C (N (Fa 3)) (N (Fa 4)))) (Fa 3)) (Fa 5) 
-         -- (((p -> q) -> (¬r -> ¬s)) -> r) -> t
+         -- (((p -> q) -> (Â¬r -> Â¬s)) -> r) -> t
 
 taut32 = C (Fa 5) (Fa 1) -- t -> p
 
@@ -122,7 +122,7 @@ modponen = C (K (Fa 1) (C (Fa 1) (Fa 2))) (Fa 2)
            -- (p ^ (p -> q)) -> q
 
 modtol = C (K (C (Fa 1) (Fa 2)) (N (Fa 2))) (N (Fa 1)) 
-         -- ((p -> q) ^ ¬q) -> ¬p
+         -- ((p -> q) ^ Â¬q) -> Â¬p
 
 peirce = C (C (C (Fa 1) (Fa 2)) (Fa 1)) (Fa 1) 
          -- ((p -> q) -> p) -> p
